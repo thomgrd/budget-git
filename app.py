4f0bd6b2be2ce2cf9b2a37cd7ee6d0e86254544e
thomgrd <128264872+thomgrd@users.noreply.github.com>
2023-03-28 18:28:59 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+from flask import Flask, jsonify, request, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
-client = MongoClient('localhost', 27017)
-db = client['mydatabase']
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://thomscustom:<neele>@cluster0.b3iilpr.mongodb.net/?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/budget_app"
 mongo = PyMongo(app)
 
-@app.route('/')
+@app.route("/")
 def home():
-    budget = mongo.db.budget.find_one()
-    expenses = mongo.db.expenses.find()
-    total_expenses = sum([expense['amount'] for expense in expenses])
-    remaining_budget = budget['amount'] - total_expenses
-    return render_template('home.html', budget=budget['amount'], expenses=expenses, total_expenses=total_expenses, remaining_budget=remaining_budget)
+    return "Hello, World!"
 
-@app.route('/budget', methods=['POST'])
-def save_budget():
-    budget = request.form['budget']
-    mongo.db.budget.update_one({}, {'$set': {'amount': int(budget)}}, upsert=True)
-    return redirect(url_for('home'))
-
-@app.route('/')
-def index():
-    mycollection = db['budgets']
-    document = {'name': 'John', 'age': 30}
-    mycollection.insert_one(document)
-    return 'Document inserted.'
-
-
-budgets = mongo.db.budgets.find()
+@app.route("/budget", methods=["GET", "POST"])
+def budgets():
+    if request.method == "GET":
+        budgets = list(mongo.db.budget.find())
+        for budget in budgets:
+            budget["_id"] = str(budget["_id"])
+        return render_template('budgets.html', budgets=budgets)
+    elif request.method == "POST":
+        data = request.get_json()
+        result = mongo.db.budget.insert_one(data)
+        return jsonify({"inserted_id": str(result.inserted_id)})
+  
+@app.route('/budget')
+def budgets():
+    budgets = mongo.db.budget.find()
+    return render_template('budget.html', budget=budgets)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
 
 
