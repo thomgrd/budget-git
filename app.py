7b9af71b346ab9a20bf8077acd0ed3e36467c033
thomgrd <128264872+thomgrd@users.noreply.github.com>
2023-03-28 16:59:50 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://thomscustom:<Neele21122009>@cluster0.b3iilpr.mongodb.net/?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
-    return 'Hello, world!'
+    budget = mongo.db.budget.find_one()
+    expenses = mongo.db.expenses.find()
+    total_expenses = sum([expense['amount'] for expense in expenses])
+    remaining_budget = budget['amount'] - total_expenses
+    return render_template('home.html', budget=budget['amount'], expenses=expenses, total_expenses=total_expenses, remaining_budget=remaining_budget)
+
+@app.route('/budget', methods=['POST'])
+def save_budget():
+    budget = request.form['budget']
+    mongo.db.budget.update_one({}, {'$set': {'amount': int(budget)}}, upsert=True)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
